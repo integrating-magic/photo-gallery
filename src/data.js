5adbcfd85b41b0f2ae787fd72a09bf0379dc53cd
Jeremy Brown <jlbmagic@gmail.com>
2022-02-05 07:10:45 -0500

    initial set up

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const data = {
+  data: [
+    {
+      fieldData: {
+        ArchiveURL:
+          "https://s3.amazonaws.com/florio-management-test-bucket/PROD/NOTE/richard.hodgson_63777430266.JPG",
+        ArchiveURL_thumb:
+          "https://s3.amazonaws.com/florio-management-test-bucket/PROD/NOTE_THUMB/richard.hodgson_63777430266.JPG",
+        Image: "",
+        Note: "",
+        isPhotoNote: "1",
+        zzCreatedBy: "richard.hodgson",
+        zzCreatedByDisplay: "richard.hodgson",
+        zzCreatedByWeb: "",
+        zzModBy: "richard.hodgson",
+        zzModifiedByWeb: "",
+        zz_containerdata_filesize: 0,
+        zzid: "321219C2-0DC3-475C-AAC5-F5C2DB99999F",
+        zzid_LESE: "",
+        zzid_MAIN: "A801E5C5-3835-4672-B79E-3A825A3E3463",
+        zzid_PEPL: "",
+      },
+      modId: "3",
+      portalData: {},
+      recordId: "551707",
+    },
+    {
+      fieldData: {
+        ArchiveURL:
+          "https://s3.amazonaws.com/florio-management-test-bucket/PROD/NOTE/richard.hodgson_63777430831.JPG",
+        ArchiveURL_thumb:
+          "https://s3.amazonaws.com/florio-management-test-bucket/PROD/NOTE_THUMB/richard.hodgson_63777430831.JPG",
+        Image: "",
+        Note: "",
+        isPhotoNote: "1",
+        zzCreatedBy: "richard.hodgson",
+        zzCreatedByDisplay: "richard.hodgson",
+        zzCreatedByWeb: "",
+        zzModBy: "richard.hodgson",
+        zzModifiedByWeb: "",
+        zz_containerdata_filesize: 0,
+        zzid: "79DFA0A4-3F48-4F08-AA91-782A70CFAD12",
+        zzid_LESE: "",
+        zzid_MAIN: "A801E5C5-3835-4672-B79E-3A825A3E3463",
+        zzid_PEPL: "",
+      },
+      modId: "3",
+      portalData: {},
+      recordId: "551727",
+    },
+  ],
+  dataInfo: {
+    database: "JCM_Development_jeremy Recovered",
+    foundCount: 4,
+    layout: "web_note",
+    returnedCount: 4,
+    table: "Notes__NOTE",
+    totalRecordCount: 443211,
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,77 @@
-window.loadGallery = (obj) => {
-  const o = JSON.parse(obj);
-  const images = o.data;
+const jquery = require("jquery");
+window.$ = window.jQuery = jquery;
+import lightGallery from "lightgallery";
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+import { data } from "./data.js";
+
+const generateElementResponsive = (w, h, url, thumb, caption) => {
+  const a = ` <a data-lg-size="${w}-${h}" class="gallery-item" data-src="${url}" data-sub-html="<h4>${caption}</h4>class="img-responsive" src="${url}" />
+</a>`;
+};
+
+function getMeta(url) {
+  var img = new Image(url);
+  img.addEventListener("load", function () {
+    alert(this.naturalWidth + " " + this.naturalHeight);
+  });
+  img.src = url;
+  return { width: this.naturalWidth, height: this.naturalHeight };
+}
+const genertateImages = (url, thumb, caption) => {
+  // const dimensions = getMeta(url);
+  // console.log(dimensions);
+  // const width = dimensions.width;
+  // const height = dimensions.height;
+  // return;
+  const a = ` <a href="${url}">
+<img alt="${caption}" src="${thumb}">
+</a>
+`;
+
+  return a;
+};
+
+// const o = JSON.parse(obj);
+// const images = o.data;
+// return;
+// const items = images.map((image) => {
+//   const o = {};
+//   o.src = "data:image/png;base64," + image.fieldData.b64;
+//   o.thumb = "data:image/png;base64," + image.fieldData.Thumb;
+//   o.w = image.fieldData.width;
+//   o.h = image.fieldData.height;
+//   console.log(o);
+//   return o;
+// });
+// console.log(items);
+// const $dynamicGallery = document.getElementById("dynamic-gallery-demo");
+// const dynamicGallery = document.getElementById("dynamicGallery");
+// lightGallery($dynamicGallery, {
+//   plugins: [lgThumbnail, lgZoom],
+//   dynamic: true,
+//   licenseKey: "EB7B1732-E7704360-A97F0905-0C0C3536",
+//   dynamicEl: items,
+// });
+
+window.loadGallery = () => {
+  const images = data.data;
   console.log(images);
-  //   return;
-  const items = images.map((image) => {
-    const o = {};
-    o.src = "data:image/png;base64," + image.fieldData.b64;
-    o.w = image.fieldData.width;
-    o.h = image.fieldData.height;
-    return o;
+  const gallery = document.getElementById("lightgallery");
+
+  images.forEach((image) => {
+    const url = image.fieldData.ArchiveURL;
+    const thumb = image.fieldData.ArchiveURL_thumb;
+    const caption = image.fieldData.zzCreatedByDisplay;
+    const a = genertateImages(url, thumb, caption);
+
+    gallery.insertAdjacentHTML("beforeend", a);
+  });
+
+  lightGallery(gallery, {
+    plugins: [lgZoom, lgThumbnail],
+    licenseKey: "your_license_key",
+    speed: 500,
+    download: false,
   });
 };
