026d4106730cb6ce74d47133638ee518dd7128ed
Jeremy Brown <jlbmagic@gmail.com>
2022-02-06 08:03:51 -0500

    major changes

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,7 +1,7 @@
 const open = require("open");
 const path = require("path");
 const server = "$";
-const file = "jsDev";
+const file = "PhotoGallery";
 const uploadScript = "UploadToHTML";
 
 const fileUrl = `fmp://${server}/${file}?script=${uploadScript}&param=`;
--- src/index.js
+++ src/index.js
@@ -1,83 +1,109 @@
-const jquery = require("jquery");
-window.$ = window.jQuery = jquery;
-import lightGallery from "lightgallery";
-import lgThumbnail from "lightgallery/plugins/thumbnail";
-import lgZoom from "lightgallery/plugins/zoom";
+// const jquery = require("jquery");
+// window.$ = window.jQuery = jquery;
+// import lightGallery from "lightgallery";
+// // import justifiedGallery from "jquery.justifiedGallery";
+// import lgThumbnail from "lightgallery/plugins/thumbnail";
+// import lgZoom from "lightgallery/plugins/zoom";
 import { data } from "./data.js";
 
 const generateElementResponsive = (w, h, url, thumb, caption) => {
   const a = ` <a data-lg-size="${w}-${h}" class="gallery-item" data-src="${url}" data-sub-html="<h4>${caption}</h4>class="img-responsive" src="${url}" />
 </a>`;
 };
-
-function getMeta(url) {
-  var img = new Image(url);
-  img.addEventListener("load", function () {
-    alert(this.naturalWidth + " " + this.naturalHeight);
-  });
+const genImage = (url, thumb, createdBy, note) => {
+  const a = `<a
+  href="${url}"
+  data-src="${url}"
+  data-sub-html=".caption"
+  class="gallery-item"
+>
+  <img
+    alt="Buddy.buchtler"
+    src=${thumb}"
+  />
+  <div class="caption" style="display: none">
+    <h4>${caption}</h4>
+  </div>
+</a>`;
+  return a;
+};
+async function getMeta(url) {
+  const img = new Image();
   img.src = url;
-  return { width: this.naturalWidth, height: this.naturalHeight };
+  await img.decode();
+  // console.log(`width: ${img.width}, height: ${img.height}`);
+  return img;
 }
-const genertateImages = (url, thumb, caption) => {
-  // const dimensions = getMeta(url);
-  // console.log(dimensions);
-  // const width = dimensions.width;
-  // const height = dimensions.height;
-  // return;
-  const a = ` <a href="${url}" data-sub-html=".caption">
-<img alt="${caption}" src="${thumb}" /> <div class="caption" style="display:none">
-<h4>Caption1</h4>
-<p>Desc1</p>
+const genertateImages = (url, thumb, createdBy, note, id, dim) => {
+  console.log("GEN", dim);
+  const a = ` <a  id = ${id} data-src=${url}  data-lg-size="1600-2400" data-sub-html=".caption" class="gallery-item">
+<img alt="${createdBy}  - ${note}" src="${url}" /> <div class="caption" style="display:none">
+
 </div>
 </a>
 `;
 
   return a;
 };
-
-// const o = JSON.parse(obj);
-// const images = o.data;
-// return;
-// const items = images.map((image) => {
-//   const o = {};
-//   o.src = "data:image/png;base64," + image.fieldData.b64;
-//   o.thumb = "data:image/png;base64," + image.fieldData.Thumb;
-//   o.w = image.fieldData.width;
-//   o.h = image.fieldData.height;
-//   console.log(o);
-//   return o;
-// });
-// console.log(items);
-// const $dynamicGallery = document.getElementById("dynamic-gallery-demo");
-// const dynamicGallery = document.getElementById("dynamicGallery");
-// lightGallery($dynamicGallery, {
-//   plugins: [lgThumbnail, lgZoom],
-//   dynamic: true,
-//   licenseKey: "EB7B1732-E7704360-A97F0905-0C0C3536",
-//   dynamicEl: items,
-// });
-
-window.loadGallery = () => {
-  const images = data.data;
-  console.log(images);
+const defaultConfig = {
+  displayThumbnail: false,
+  captions: true,
+  rowHeight: 180,
+  margins: 5,
+  lastRow: "left",
+  randomize: false,
+};
+window.loadGallery = (fmObj) => {
+  const o = JSON.parse(fmObj);
+  const fmData = o.data;
+  const config = o.config || defaultConfig;
+  // console.log(fmData);
+  // const obj = JSON.parse(fmData);
+  const obj = data;
+  const images = obj.data;
+  // console.log(images);
   const gallery = document.getElementById("lightgallery");
-
-  images.forEach((image) => {
+  let divs = [];
+  const newImages = images.slice(0, 100);
+  newImages.forEach(async (image) => {
     const url = image.fieldData.ArchiveURL;
+    const img = new Image();
+    img.src = url;
+    // await img.decode();
+    const dim = { width: 960, height: 1280 };
     const thumb = image.fieldData.ArchiveURL_thumb;
-    const caption = image.fieldData.zzCreatedByDisplay;
-    const a = genertateImages(url, thumb, caption);
+    const id = image.fieldData.zzid;
+    const createdBy = "Image taken by: " + image.fieldData.zzCreatedByDisplay;
+    const note = image.fieldData.Note || "No note";
+    const a = genertateImages(url, thumb, createdBy, note, id, dim);
 
     gallery.insertAdjacentHTML("beforeend", a);
   });
 
-  lightGallery(gallery, {
-    plugins: [lgZoom, lgThumbnail],
-    licenseKey: "your_license_key",
-    speed: 500,
-    download: false,
-    allowMediaOverlap: true,
-    toggleThumb: true,
-    subHtmlSelectorRelative: true,
+  $("#lightgallery")
+    .justifiedGallery({
+      captions: config.captions,
+      lastRow: config.lastRow,
+      rowHeight: config.rowHeight,
+      margins: config.margins,
+      randomize: config.randomize,
+    })
+    .on("jg.complete", function () {
+      window.lightGallery(document.getElementById("lightgallery"), {
+        plugins: [lgZoom, lgThumbnail],
+        // mobileSettings: {
+        //   controls: false,
+        //   showCloseIcon: false,
+        //   download: false,
+        //   rotate: false,
+        // },
+      });
+    });
+
+  $(".gallery-item").on("click", function (e) {
+    const id = e.currentTarget.id;
+    console.log(id);
+    const obj = { id };
+    FileMaker.PerformScript("Open Image ({id})", JSON.stringify(obj));
   });
 };
